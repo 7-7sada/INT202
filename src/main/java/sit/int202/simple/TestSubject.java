e8c65b43626885cc0679919db34cdc65e7fd55ab
Jedsada Chotinun <jasda.jair9un@mail.kmutt.ac.th>
2022-08-24 12:25:39 +0700

    update:week2

--- src/main/java/sit/int202/simple/TestSubject.java
+++ src/main/java/sit/int202/simple/TestSubject.java
@@ -1,8 +1,10 @@
 package sit.int202.simple;
 
+import sit.int202.simple.entities.Subject;
+
 public class TestSubject {
     public static void main(String[] args) {
-        Subject subject = new Subject("INT204","Server Side !",2.0);
+        Subject subject = new Subject("INT204","Server Side !",2);
         System.out.printf("Id: %s, Title: %s, Credit: %f\n",
                 subject.getId(), subject.getTitle(), subject.getCredit());
     }
